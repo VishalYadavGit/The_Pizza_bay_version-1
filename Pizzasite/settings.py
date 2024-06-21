41119043bb21257852d58d350d1a6004ae72bab7
Xegendvy <vystudent68@gmail.com>
2024-06-22 00:02:56 +0530

    db config

--- Pizzasite/settings.py
+++ Pizzasite/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'Pizzasite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ghnirazj',
+        'USER': 'ghnirazj',
+        'PASSWORD': 'Vn9QzorQOcYDubLn5GKlpcXe8byTlfn6',
+        'HOST': 'localhost', 
+        'PORT': '5432', 
     }
 }
 
